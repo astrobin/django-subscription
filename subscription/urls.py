04667e2e1d50da8b1d6e217e0219d3136bce4074
Salvatore Iovene <salvatore@iovene.com>
2019-12-26 21:44:10 +0100

    Update urls for Django 1.10 compatibility

--- subscription/urls.py
+++ subscription/urls.py
@@ -1,18 +1,20 @@
-from django.conf.urls import patterns, url, include
+from django.conf.urls import url, include
 from django.views.generic import TemplateView
 
-urlpatterns = patterns('subscription.views',
-url(r'^done/', TemplateView.as_view(template_name='subscription/subscription_done.html'), name='subscription_done'),
-url(r'^change-done/', TemplateView.as_view(template_name='subscription/subscription_change_done.html'), name='subscription_change_done'),
-url(r'^cancel/', TemplateView.as_view(template_name='subscription/subscription_cancel.html'), name='subscription_cancel'),
+urlpatterns = (
+    url(r'^done/', TemplateView.as_view(template_name='subscription/subscription_done.html'), name='subscription_done'),
+    url(r'^change-done/', TemplateView.as_view(template_name='subscription/subscription_change_done.html'),
+        name='subscription_change_done'),
+    url(r'^cancel/', TemplateView.as_view(template_name='subscription/subscription_cancel.html'),
+        name='subscription_cancel'),
 )
 
-urlpatterns += patterns('subscription.views',
+urlpatterns += (
     (r'^$', 'subscription_list', {}, 'subscription_list'),
     (r'^(?P<object_id>\d+)/$', 'subscription_detail', {}, 'subscription_detail'),
     (r'^(?P<object_id>\d+)/(?P<payment_method>(standard|pro))/$', 'subscription_detail', {}, 'subscription_detail'),
 )
 
-urlpatterns += patterns('',
+urlpatterns += (
     (r'^paypal/', include('paypal.standard.ipn.urls')),
 )
