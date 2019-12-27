9502f40f88ad4e8a14efee7fe2132657409561bb
Salvatore Iovene <salvatore@iovene.com>
2019-12-27 12:27:11 +0100

    Explicitly use the url object

--- subscription/urls.py
+++ subscription/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url, include
 from django.views.generic import TemplateView
 
+from .views import subscription_detail, subscription_list
+
 urlpatterns = (
     url(r'^done/', TemplateView.as_view(template_name='subscription/subscription_done.html'), name='subscription_done'),
     url(r'^change-done/', TemplateView.as_view(template_name='subscription/subscription_change_done.html'),
@@ -10,11 +12,11 @@ urlpatterns = (
 )
 
 urlpatterns += (
-    (r'^$', 'subscription_list', {}, 'subscription_list'),
-    (r'^(?P<object_id>\d+)/$', 'subscription_detail', {}, 'subscription_detail'),
-    (r'^(?P<object_id>\d+)/(?P<payment_method>(standard|pro))/$', 'subscription_detail', {}, 'subscription_detail'),
+    url(r'^$', subscription_list, {}, 'subscription_list'),
+    url(r'^(?P<object_id>\d+)/$', subscription_detail, {}, 'subscription_detail'),
+    url(r'^(?P<object_id>\d+)/(?P<payment_method>(standard|pro))/$', subscription_detail, {}, 'subscription_detail'),
 )
 
 urlpatterns += (
-    (r'^paypal/', include('paypal.standard.ipn.urls')),
+    url(r'^paypal/', include('paypal.standard.ipn.urls')),
 )
